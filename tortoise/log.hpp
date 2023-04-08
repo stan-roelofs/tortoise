1132127f6ea329b9f3876a14accc1ad20b770db4
Stan Roelofs <stan@stanroelofs.nl>
2023-04-08 12:52:12 +0200

    Improved logging

--- tortoise/log.hpp
+++ tortoise/log.hpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 #ifdef TORTOISE_ENABLE_LOGGING
-#define LOG(tag, s, ...) printf(tag " " s "\n", ##__VA_ARGS__)
+#define LOG(tag, s, ...) printf("[" tag "] - " s "\n", ##__VA_ARGS__)
 #else
 #define LOG(...) ((void)0)
 #endif
